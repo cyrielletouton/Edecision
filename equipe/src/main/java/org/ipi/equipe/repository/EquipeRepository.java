f0a60e7d0c21b2f3cc387a4abf20b4578e0d65e9
Raphael Perles <progradius@protonmail.com>
2022-11-17 13:23:34 +0100

    classes method and proposition repo

--- equipe/src/main/java/org/ipi/equipe/repository/EquipeRepository.java
+++ equipe/src/main/java/org/ipi/equipe/repository/EquipeRepository.java
@@ -1,10 +1,10 @@
-package org.ipi.proposition.repository;
+package org.ipi.equipe.repository;
 
-import org.ipi.proposition.model.Proposition;
+import org.ipi.equipe.model.Equipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PropositionRepository extends JpaRepository<Proposition, Long> {
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
 }
