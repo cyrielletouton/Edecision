4fd3d09a4bc18a4aa5fc617e0a5100d5c2e06bfc
mariev619 <marie.vendran@gmail.com>
2022-11-20 18:45:23 +0100

    MappedSuperClass annotation for the abstract class

--- proposition/src/main/java/org/ipi/proposition/model/PropositionGenerale.java
+++ proposition/src/main/java/org/ipi/proposition/model/PropositionGenerale.java
@@ -1,11 +1,9 @@
 package org.ipi.proposition.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@MappedSuperclass
 public abstract class PropositionGenerale {
 
     @Id
