b08b039bbc6aa6f5571fd987df9dc5b23786faab
mariev619 <marie.vendran@gmail.com>
2022-12-12 15:55:54 +0100

    CRUD works with templates for projet

--- projet/src/main/java/org/ipi/projet/controller/ProjetContoller.java
+++ projet/src/main/java/org/ipi/projet/controller/ProjetContoller.java
@@ -3,47 +3,81 @@ package org.ipi.projet.controller;
 import org.ipi.projet.model.Projet;
 import org.ipi.projet.repository.ProjetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
 
-@RestController
+@Controller
 public class ProjetContoller {
     @Autowired
     private ProjetRepository projetRepository;
 
     @GetMapping("/projets")
-    public List<Projet> voirProjets(Model out){
+    public String voirProjets(Model out){
         List<Projet> projets = projetRepository.findAll();
         out.addAttribute("projets", projets);
-        return projets;
+        return "voir-projets";
     }
 
     @GetMapping("/projet/{id}")
-    public Projet voirProjetParId(Model out,@PathVariable Long id) {
+    public String voirProjetParId(Model out,@PathVariable Long id) {
         Projet projet = projetRepository.findById(id).get();
         out.addAttribute("projet",projet);
-        return projet;
+        return "voir-projet";
     }
 
-    @PostMapping("/creer-projet")
-    public Projet creerProjet(@RequestBody Projet projet) {
-        return projetRepository.save(projet);
+    @GetMapping("/projets/creer-projet")
+    public String creerProjetGet(Model out) {
+        List<Projet> projets = projetRepository.findAll();
+        HashSet<String> equipes = new HashSet<>();
+        HashSet<String> propositions = new HashSet<>();
+        for (Projet projet : projets) {
+            equipes.addAll(projet.getEquipes());
+            propositions.addAll(projet.getPropositions());
+        }
+        out.addAttribute("equipes", equipes);
+        out.addAttribute("propositions", propositions);
+        out.addAttribute("projet", new Projet());
+        return "creer-projet";
     }
 
-    @PutMapping("/mettre-a-jour-projet/{id}")
-    public Projet aJourProjet(@RequestBody Projet projet, @PathVariable Long id) {
+    @PostMapping("/projets/creer-projet")
+    public String creerProjet(@ModelAttribute Projet projet) {
+        projetRepository.save(projet);
+        return "redirect:/projets";
+    }
+
+    @GetMapping("/projets/modifier-projet/{id}")
+    public String modifierProjetGet(Model out, @PathVariable Long id) {
+        Projet projet = projetRepository.findById(id).get();
+        List<Projet> projets = projetRepository.findAll();
+        HashSet<String> equipes = new HashSet<>();
+        HashSet<String> propositions = new HashSet<>();
+        for (Projet prj : projets) {
+            equipes.addAll(prj.getEquipes());
+            propositions.addAll(prj.getPropositions());
+        }
+        out.addAttribute("equipesList", equipes);
+        out.addAttribute("propositionsList", propositions);
+        out.addAttribute("projet", projet);
+        return "modifier-projet";
+    }
+    @PostMapping("/projets/modifier-projet")
+    public String aJourProjet(@ModelAttribute Projet projet, @RequestParam Long id) {
         Projet projetAJour = projetRepository.findById(id).get();
         projetAJour.setNom(projet.getNom());
         projetAJour.setEquipes(projet.getEquipes());
         projetAJour.setPropositions(projet.getPropositions());
-        return projetRepository.save(projetAJour);
+        projetRepository.save(projetAJour);
+        return "redirect:/projets";
     }
 
-    @DeleteMapping("projet/{id}")
-    public boolean supprimerProjet(@PathVariable Long id){
+    @GetMapping("/projets/supprimer-projet/{id}")
+    public String supprimerProjet(@PathVariable Long id){
         projetRepository.deleteById(id);
-        return true;
+        return "redirect:/projets";
     }
 }
--- projet/src/main/java/org/ipi/projet/repository/ProjetRepository.java
+++ projet/src/main/java/org/ipi/projet/repository/ProjetRepository.java
@@ -4,7 +4,8 @@ import org.ipi.projet.model.Projet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjetRepository extends JpaRepository<Projet, Long> {
-
 }
