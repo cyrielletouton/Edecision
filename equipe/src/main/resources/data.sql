684f98f7465856ef64b6e972e642574e4dd6cb36
Raphael Perles <progradius@protonmail.com>
2023-04-12 19:21:50 +0200

    removing useless table in script

--- equipe/src/main/resources/data.sql
+++ equipe/src/main/resources/data.sql
@@ -7,16 +7,3 @@ INSERT INTO projet (projet_id,equipe_id)
 VALUES
     ('1','1'),
     ('1','2');
-
-INSERT INTO membres (membre_id,equipe_id)
-VALUES
-    ('1', '1'),
-    ('2', '1'),
-    ('3', '1'),
-    ('4', '1'),
-    ('5', '1'),
-    ('6', '2'),
-    ('7', '2'),
-    ('8', '2'),
-    ('9', '2'),
-    ('10', '2');
