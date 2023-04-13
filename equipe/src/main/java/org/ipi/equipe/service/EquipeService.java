5fd7e911d2f05552f41bd4a6d05d189dd2ef7bac
Raphael Perles <progradius@protonmail.com>
2023-04-13 18:52:05 +0200

    compositionequipe can fetch projects

--- equipe/src/main/java/org/ipi/equipe/service/EquipeService.java
+++ equipe/src/main/java/org/ipi/equipe/service/EquipeService.java
@@ -1,6 +1,7 @@
 package org.ipi.equipe.service;
 
 import org.ipi.equipe.model.CompositionEquipe;
+import org.ipi.equipe.model.dto.ProjetDTO;
 import org.ipi.equipe.model.entity.Equipe;
 import org.ipi.equipe.model.dto.MembresDTO;
 import org.ipi.equipe.repository.EquipeRepository;
@@ -19,12 +20,15 @@ public class EquipeService {
     private String apiGateway;
     @Value("${api.membres}")
     private String membresApi;
+    @Value("${api.projet}")
+    private String projetApi;
     RestTemplate restTemplate = new RestTemplate();
     @Autowired
     private EquipeRepository equipeRepository;
 
     public CompositionEquipe compositionEquipeService(Long id) {
         List<Long> membresIds = new ArrayList<>();
+        List<Long> projetsId = new ArrayList<>();
         CompositionEquipe composition = new CompositionEquipe();
 
         Equipe equipe = equipeRepository.findById(id).get();
@@ -42,7 +46,16 @@ public class EquipeService {
         }
         composition.setMembres(membresIds);
         //TODO: Projets, in the same way as Membres
-
+        ProjetDTO[] projets = restTemplate.getForEntity(apiGateway + projetApi + "get", ProjetDTO[].class).getBody();
+        for (ProjetDTO projet : projets) {
+            Long projetId = projet.getId();
+            for (Long equipeId : projet.getEquipes()) {
+                if( equipeId == id){
+                    projetsId.add(projetId);
+                }
+            }
+        }
+        composition.setProjets(projetsId);
         return composition;
     }
 }
