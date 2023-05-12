cacb5f63a36f8df1a0b798a88100a650ddc00f2e
Raphael Perles <progradius@protonmail.com>
2023-05-12 14:33:44 +0200

    data modifications in proposition

--- proposition/src/main/resources/data.sql
+++ proposition/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO propositions (titre,description,statut,estAccepte,maxVote,nbrVote, nbrabstention, proprietaire, votants, projetID)
 VALUES
-    ('Titre 1','Description','ENCOURS', false, 5, 4, 0, 1, 0, 1);
+    ('Titre 1','Description','ENCOURS', false, 5, 4, 0, 1, "1,2,3,4", 1);
 
 INSERT INTO amendements (contenu)
 VALUES
