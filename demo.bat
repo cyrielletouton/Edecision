03fe823b20cd0606d8064457ed6858ccb9812c72
MM12300 <nerggalkore@gmail.com>
2023-05-31 18:54:29 +0200

    structuring the demo

--- demo.bat
+++ demo.bat
@@ -14,5 +14,12 @@ curl -X POST http://localhost:8080/api/equipe/give/3/projet/2 & echo.
 echo "Project Given"
 echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
 
+echo "Creating 10 members"
+curl -X POST -H "Content-Type: application/json" -d "[{\"nom\":\"Membre A\",\"mdp\":\"passA\",\"equipe\":3},{\"nom\":\"Membre B\",\"mdp\":\"passB\",\"equipe\":3},{\"nom\":\"Membre C\",\"mdp\":\"passC\",\"equipe\":3},{\"nom\":\"Membre D\",\"mdp\":\"passD\",\"equipe\":3},{\"nom\":\"Membre E\",\"mdp\":\"passE\",\"equipe\":3},{\"nom\":\"Membre F\",\"mdp\":\"passF\",\"equipe\":4},{\"nom\":\"Membre G\",\"mdp\":\"passG\",\"equipe\":4},{\"nom\":\"Membre H\",\"mdp\":\"passH\",\"equipe\":4},{\"nom\":\"Membre I\",\"mdp\":\"passI\",\"equipe\":4},{\"nom\":\"Membre J\",\"mdp\":\"passJ\",\"equipe\":4}]" http://localhost:8080/api/membre/createMany & echo.
+echo "Members created"
+echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
 
-
+echo "Creating a proposition"
+curl -X POST -H "Content-Type: application/json" -d "{\"titre\":\"test\",\"description\":\"test\",\"statut\":\"OUVERTE\",\"estAccepte\":false,\"maxVote\":5,\"nbrVote\":0,\"nbrAbstention\":0,\"proprietaire\":11,\"projetId\":2,\"votants\":\"\"}" http://localhost:8080/api/proposition/create & echo.
+echo "Proposition created"
+echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
