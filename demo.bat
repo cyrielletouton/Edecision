73b82f813065ad0910e46ade426f05f5538ac58b
MM12300 <nerggalkore@gmail.com>
2023-06-05 16:53:36 +0200

    proposition and vote removal

--- demo.bat
+++ demo.bat
@@ -28,28 +28,3 @@ echo "Updating a proposition"
 curl -X POST -H "Content-Type: application/json" -d "{\"titre\":\"test\",\"description\":\"test\",\"statut\":\"ENCOURS\",\"estAccepte\":false,\"maxVote\":5,\"nbrVote\":0,\"nbrAbstention\":0,\"proprietaire\":11,\"projetId\":2,\"votants\":\"\"}" http://localhost:8080/api/proposition/update/2 & echo.
 echo "Proposition updated"
 echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
-
-echo "Voting : member 11 (POUR)"
-curl -X POST -H "Content-Type: application/json" -d "{\"voteStatut\":\"POUR\",\"membre\":11,\"proposition\":2,\"equipe\":3}" http://localhost:8080/api/vote/create
-echo "Proposition voted"
-echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
-
-echo "Voting : member 12 (POUR)"
-curl -X POST -H "Content-Type: application/json" -d "{\"voteStatut\":\"POUR\",\"membre\":12,\"proposition\":2,\"equipe\":3}" http://localhost:8080/api/vote/create & echo.
-echo "Proposition voted"
-echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
-
-echo "Voting : member 13 (CONTRE)"
-curl -X POST -H "Content-Type: application/json" -d "{\"voteStatut\":\"CONTRE\",\"membre\":13,\"proposition\":2,\"equipe\":3}" http://localhost:8080/api/vote/create & echo.
-echo "Proposition voted"
-echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
-
-echo "Voting : member 14 (CONTRE)"
-curl -X POST -H "Content-Type: application/json" -d "{\"voteStatut\":\"CONTRE\",\"membre\":14,\"proposition\":2,\"equipe\":3}" http://localhost:8080/api/vote/create & echo.
-echo "Proposition voted"
-echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
-
-echo "Voting : member 15 (ABSTENTION)"
-curl -X POST -H "Content-Type: application/json" -d "{\"voteStatut\":\"ABSTENTION\",\"membre\":15,\"proposition\":2,\"equipe\":3}" http://localhost:8080/api/vote/create & echo.
-echo "Proposition voted"
-echo "\-/-\-/-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/-\-/"
